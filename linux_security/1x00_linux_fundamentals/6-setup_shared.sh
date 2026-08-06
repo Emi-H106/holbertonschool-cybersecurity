b1783086c32175140c8574a159d530fd2d39584b
Emi-H106 <emi@Emi.localdomain>
2026-08-06 02:05:33 +0200

    fix command

--- linux_security/1x00_linux_fundamentals/6-setup_shared.sh
+++ linux_security/1x00_linux_fundamentals/6-setup_shared.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-mkdir -p /shared/devs
-chgrp developers /shared/devs
-chmod 775 /shared/devs
-chmod g+s /shared/devs
-chmod +t /shared/devs
+mkdir -p /shared/devs && chgrp developers /shared/devs && chmod 3775 /shared/devs
