c5d2c2aec548851dde1f4b5baac2748c829e07d7
Emi-H106 <emi@Emi.localdomain>
2026-08-06 03:23:04 +0200

    fix command2

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mkdir -p "$1" && chown root:$2 "$1" && chmod 2770 "$1" && echo "/var/log/app/*.log { daily rotate 7 create 0640 root www-data }" > /etc/logrotate.d/app
+mkdir -p "$1" && chown root:$2 "$1" && chmod 2750 "$1" && echo "/var/log/app/*.log { daily rotate 7 create 0640 root www-data }" > /etc/logrotate.d/app
