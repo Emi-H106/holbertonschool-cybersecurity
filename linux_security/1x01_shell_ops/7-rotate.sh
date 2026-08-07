4112a1949b5c394a65908da1e6ecd84ed2ca4e53
Emi-H106 <emi@Emi.localdomain>
2026-08-07 15:50:47 +0200

    fix command

--- linux_security/1x01_shell_ops/7-rotate.sh
+++ linux_security/1x01_shell_ops/7-rotate.sh
@@ -9,7 +9,7 @@ for file in "$1"/*.log
 do
     [ -f "$file" ] || continue
 
-    if [ "$(stat -c %s "$file")" -gt 1024 ]; then
+    if [ "$(stat -c%s "$file")" -gt 1024 ]; then
         gzip "$file"
         mv "$file.gz" "$1/backups/"
     else
