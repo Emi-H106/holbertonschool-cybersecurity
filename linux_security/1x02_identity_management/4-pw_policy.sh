ea63956bbc85a761e4699f5541861c1d4df26137
Emi-H106 <emi@Emi.localdomain>
2026-08-09 03:12:51 +0200

    Identity & Access Management_task4

---
+++ linux_security/1x02_identity_management/4-pw_policy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if ! dpkg -s "$1" > /dev/null 2>&1; then
+    apt-get update
+    apt-get install -y "$1"
+fi
+
+if grep -q "pam_pwquality.so" "$2"; then
+    sed -i '/pam_pwquality.so/ s/$/ minlen=12 minclass=3/' "$2"
+fi
