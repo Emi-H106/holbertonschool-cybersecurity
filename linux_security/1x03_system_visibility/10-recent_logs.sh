187b65314dbab050edabb78852e74f2770d5172f
Emi-H106 <emi@Emi.localdomain>
2026-08-12 01:25:12 +0200

    add flag

--- linux_security/1x03_system_visibility/10-recent_logs.sh
+++ linux_security/1x03_system_visibility/10-recent_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -v cutoff="$(date -d '30 minutes ago' +%s)" -v year="$(date +%Y)" '/sshd/ {cmd="date -d \"" year " " $1 " " $2 " " $3 "\" +%s"; cmd | getline t; close(cmd); if (t >= cutoff) print $0}' "$1"
+awk -v cutoff="$(date -d '30 minutes ago' +%s)" -v year="$(date +%Y)" '/sshd/ {cmd="date -d \"" $1 " " $2 " " $3 " " year "\" +%s"; cmd | getline t; close(cmd); if (t >= cutoff) print $0}' "$1"
