1b73108ebfedd8dc030bd11b5d0bddeac2ad3198
Emi-H106 <emi@Emi.localdomain>
2026-08-18 01:36:15 +0200

    fix to 1 line

--- network_security/2x00_network_fundamentals/1-dec2bin.sh
+++ network_security/2x00_network_fundamentals/1-dec2bin.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-decimal=$1
-binary=""
-
-for ((i=0; i<8; i++)); do
-    bit=$((decimal % 2))
-    binary="${bit}${binary}"
-    decimal=$((decimal / 2))
-done
-
-echo "$binary"
+decimal=$1; binary=""; for ((i=0; i<8; i++)); do bit=$((decimal % 2)); binary="${bit}${binary}"; decimal=$((decimal / 2)); done; echo "$binary"
