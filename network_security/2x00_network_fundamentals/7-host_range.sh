3204eb29bb74653699f1c2bce63ad5d598673d40
Emi-H106 <emi@Emi.localdomain>
2026-08-19 17:36:21 +0200

    delete line break

--- network_security/2x00_network_fundamentals/7-host_range.sh
+++ network_security/2x00_network_fundamentals/7-host_range.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IFS=. read -r a b c d <<< "$1"; ip=$((a<<24|b<<16|c<<8|d)); mask=$((0xFFFFFFFF << (32-$2) & 0xFFFFFFFF)); net=$((ip & mask)); broad=$((net | (~mask & 0xFFFFFFFF))); first=$((net+1)); last=$((broad-1)); printf "%d.%d.%d.%d - %d.%d.%d.%d\n" $((first>>24&255)) $((first>>16&255)) $((first>>8&255)) $((first&255)) $((last>>24&255)) $((last>>16&255)) $((last>>8&255)) $((last&255))
+IFS=. read -r a b c d <<< "$1"; ip=$((a<<24|b<<16|c<<8|d)); mask=$((0xFFFFFFFF << (32-$2) & 0xFFFFFFFF)); net=$((ip & mask)); broad=$((net | (~mask & 0xFFFFFFFF))); first=$((net+1)); last=$((broad-1)); printf "%d.%d.%d.%d - %d.%d.%d.%d" $((first>>24&255)) $((first>>16&255)) $((first>>8&255)) $((first&255)) $((last>>24&255)) $((last>>16&255)) $((last>>8&255)) $((last&255))
