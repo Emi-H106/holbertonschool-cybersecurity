4ad75796842e4d8281da10d5ae97b1de66380bc2
Emi-H106 <emi@Emi.localdomain>
2026-09-21 14:57:05 +0200

    Add main entry point to BreachCheck

--- python_for_cyber/4x00_python_security/breach_check.py
+++ python_for_cyber/4x00_python_security/breach_check.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 """BreachCheck main module."""
 
-print("BreachCheck v1.0 startup...")
+def main():
+    """Run the main program."""
+    print("BreachCheck v1.0 startup...")
+
+if __name__ == "__main__":
+    main()
+    
